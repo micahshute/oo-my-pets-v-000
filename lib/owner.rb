6431a447724632ec2c965f7c60d304ca71c470cc
Micah Shute <micah.shute@gmail.com>
2018-07-05 06:19:55 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,15 +29,15 @@ class Owner
   end
 
   def buy_cat(name)
-    self.pets << Cat.new(name)
+    self.pets[:cats] << Cat.new(name)
   end
 
   def buy_dog(name)
-    self.pets << Dog.new(name)
+    self.pets[:dogs] << Dog.new(name)
   end
 
   def buy_fish(name)
-    self.pets << Fish.new(name)
+    self.pets[:fishes] << Fish.new(name)
   end
 
   def say_species
