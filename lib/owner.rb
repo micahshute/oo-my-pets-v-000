a5fb74ab5c38781739f0c01a256fcfee271eda5b
Micah Shute <micah.shute@gmail.com>
2018-07-05 05:59:06 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
+require_relative 'cat'
+require_relative 'dog'
+require_relative 'fish'
+
 class Owner
-  # code goes here
+
+  attr_accessor :pets
+  
+  def initialize()
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def buy_cat(name)
+    self.pets << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    self.pets << Dog.new(name)
+  end
+
+  def buy_fishname)
+    self.pets << Fish.new(name)
+  end
+  
 end
